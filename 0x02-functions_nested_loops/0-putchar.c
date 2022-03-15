b4b61b04c8d8993345fbd49920b0f0df935309e6
Mohamed Lamine BAMBA <limto14@gmail.com>
2022-03-15 22:51:14 +0100

    a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Alwas 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
