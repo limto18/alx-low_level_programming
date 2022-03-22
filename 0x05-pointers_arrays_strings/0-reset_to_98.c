ed6b749cf55ac307c258513dc7932e304e07e50a
Mohamed Lamine BAMBA <limto14@gmail.com>
2022-03-22 23:50:21 +0100

    Task 0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - main
- *
- *@n: - pointer to int 
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to 98
+ * @n: pointer to the variable to update
  */
 void reset_to_98(int *n)
 {
