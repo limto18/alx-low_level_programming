7bb1c1f6a1bb65cf12863ff1cd1e83a227ac2690
Mohamed Lamine BAMBA <limto14@gmail.com>
2022-03-23 00:05:48 +0100

    Task2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - displays the lenth of the string
+ * @s: the number
+ * Return: returns 0
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
