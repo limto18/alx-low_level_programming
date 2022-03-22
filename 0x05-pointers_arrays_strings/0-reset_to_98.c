01171bce47cdc1ad552d5bfed2d44d6bd096de91
Mohamed Lamine BAMBA <limto14@gmail.com>
2022-03-22 23:29:39 +0100

    Task 0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,10 @@
  * reset_to_98 takes a pointer 
  * to an int as parameter and updates
  * the value it points to to 98
- *@n: int to asign
- *
+ *@n: - pointer to int 
  * Return: 0 (Success)
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
