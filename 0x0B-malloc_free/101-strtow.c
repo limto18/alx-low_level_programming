f89ffb3679bf60c33202de3b32a23a97c3aa6fc0
limto18 <limto14@gmail.com>
2022-04-06 17:57:10 -0700

    Task 6 revisited

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,20 +12,6 @@ int count_word(char *s)
 	int flag, c, w;
 
 	flag = 0;
-#include <stdlib.h>
-#include "main.h"
-
-/**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
- *
- * Return: number of words
- */
-int count_word(char *s)
-{
-	int flag, c, w;
-
-	flag = 0;
 	w = 0;
 
 	for (c = 0; s[c] != '\0'; c++)
