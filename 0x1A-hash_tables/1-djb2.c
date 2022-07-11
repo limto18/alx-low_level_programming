b5f436e09d445bf84e90b35d0af1cf77a0e14cf9
Mohamed Lamine BAMBA <limto14@gmail.com>
2022-07-11 19:14:52 +0100

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,19 +6,7 @@
  *
  * Return: hash index corresponding to the string
  */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
 
-#include "hash_tables.h"
-
-/**
- * hash_djb2 - hash function implementing the djb2 algorithm
- * @str: string to hash
- *
- * Return: hash index corresponding to the string
- */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
